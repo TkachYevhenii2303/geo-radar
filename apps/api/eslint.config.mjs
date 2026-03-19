b03c80d13259a045f9bd75005656e3b81cf73f30
TkachYevhenii2303 <tkach.yevhenii23@gmail.com>
2026-03-19 17:12:57 +0200

    feat(eslint): enhance ESLint configuration for API and UI applications
    - Added TypeScript parser options to ESLint configuration for better TypeScript support.
    - Introduced a lint script in package.json for easier linting of TypeScript files.
    - Updated dependencies to include latest versions of eslint and typescript-eslint.
    - Modified UI ESLint rules to disable additional checks for empty object types and state updates in effects.

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig([
   ...tseslint.configs.recommended,
@@ -9,6 +13,12 @@ export default defineConfig([
     'coverage/**',
   ]),
   {
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: __dirname,   // вказуємо на apps/api/
+        project: './tsconfig.json',
+      },
+    },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
--- apps/ui/eslint.config.mjs
+++ apps/ui/eslint.config.mjs
@@ -15,6 +15,8 @@ export default defineConfig([
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "react-hooks/set-state-in-effect": "off",
     },
   },
 ]);
